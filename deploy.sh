afdcbfdca2b0574598f7585294d1e5afec7db357
Bernhard Froehler <bernhard@bfroehler.info>
2013-05-08 23:30:13 +0200

    Remodel deploy to more versatile shell script

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+name=bfstop
+version=0.9.10
+
+component_name=com_${name}
+site_files=site/*
+admin_files=admin/*
+admlang_files=admlang/*
+zipfile_name=${component_name}-${version}.zip
+src_files="${name}.xml ${site_files} ${admin_files} ${admlang_files}"
+
+if [ "$1" == "zip" ]
+then
+	rm -f ${zipfile_name}
+	zip -r ${zipfile_name} ${src_files}
+	exit
+fi
+
+if [ "$1" != "" ]
+then
+	dstdir=$1
+fi
+
+if [ "$dstdir" == "" ]
+then
+	echo "You have to set dstdir variable first (to the joomla directory you want to deploy to)"
+	exit
+fi
+
+cp -r ${site_files} ${dstdir}/components/${component_name}/
+cp -r ${admin_files} ${dstdir}/administrator/components/${component_name}/
+cp -r ${admlang_files} ${dstdir}/administrator/language/
+
+
