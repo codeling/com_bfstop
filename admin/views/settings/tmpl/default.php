c2a883559a28bdda6a517797e871e289ae566d83
Bernhard Froehler <bernhard@bfroehler.info>
2013-06-23 11:16:07 +0200

    Translates settings view message

--- admin/views/settings/tmpl/default.php
+++ admin/views/settings/tmpl/default.php
@@ -1,6 +1,5 @@
 <div class="message" >
-Check the notification configuration of the bfstop plugin by clicking the button in the toolbar!<br/>
-More settings will be added here in the future, stay tuned for next bfstop versions.
+<?php echo JText::_('SETTINGS_VIEW_HINT'); ?>
 </div>
 <form method="post" name="adminForm" id="adminForm">
 	<input type="hidden" name="task" value="settings.testNotify" />
