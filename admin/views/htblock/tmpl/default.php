4cf47f34b952a6c300c29980f9eeb7462e532925
Bernhard Froehler <bernhard@bfroehler.info>
2021-03-06 23:04:30 +0100

    Remove unused(?) tooltip library loading

--- admin/views/htblock/tmpl/default.php
+++ admin/views/htblock/tmpl/default.php
@@ -6,7 +6,6 @@
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
 **/
 defined('_JEXEC') or die;
-JHtml::_('behavior.tooltip');
 ?>
 <form method="post" name="adminForm" id="adminForm">
 	<fieldset class="adminform">
