1ebe7659f8a25bd872da69685a7cfe6ce8890e79
Bernhard Froehler <bernhard@bfroehler.info>
2023-11-06 23:09:35 +0100

    Add missing header in install script

--- installscript.php
+++ installscript.php
@@ -1,5 +1,10 @@
 <?php
-// No direct access to this file
+/*
+ * @package BFStop Component (com_bfstop) for Joomla! >= 3
+ * @author Bernhard Froehler
+ * @copyright (C) Bernhard Froehler
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+**/
 defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Installer\InstallerAdapter;
