6a574d264282d0bd26832d417cf380b048c42233
Bernhard Froehler <bernhard@bfroehler.info>
2023-11-22 20:42:16 +0100

    Unify defined('_JEXEC') checks

--- installscript.php
+++ installscript.php
@@ -5,7 +5,7 @@
  * @copyright (C) Bernhard Froehler
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
 **/
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
 use Joomla\CMS\Installer\InstallerAdapter;
 
--- site/bfstop.php
+++ site/bfstop.php
@@ -5,7 +5,7 @@
  * @copyright (C) Bernhard Froehler
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
 **/
-defined('_JEXEC') or die('Direct Access to this script is not allowed.');
+defined('_JEXEC') or die;
 
 // Require base controller:
 jimport('joomla.application.component.controller');
