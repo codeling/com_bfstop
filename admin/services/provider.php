73b8bae665c5bf9730989015ca1b21f43939cd3f
Bernhard Froehler <bernhard@bfroehler.info>
2026-08-25 00:12:29 +0200

    Add component service providers, Extension classes, and manifest rewrite

---
+++ admin/services/provider.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * @package BFStop Component (com_bfstop) for Joomla!
+ * @author Bernhard Froehler
+ * @copyright (C) Bernhard Froehler
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+**/
+
+defined('_JEXEC') or die;
+
+use Codeling\Component\Bfstop\Administrator\Extension\BfstopComponent;
+use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
+use Joomla\CMS\Extension\ComponentInterface;
+use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
+use Joomla\CMS\Extension\Service\Provider\MVCFactory;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+
+return new class () implements ServiceProviderInterface {
+	public function register(Container $container)
+	{
+		$container->registerServiceProvider(new MVCFactory('\\Codeling\\Component\\Bfstop'));
+		$container->registerServiceProvider(new ComponentDispatcherFactory('\\Codeling\\Component\\Bfstop'));
+
+		$container->set(
+			ComponentInterface::class,
+			function (Container $container) {
+				$component = new BfstopComponent($container->get(ComponentDispatcherFactoryInterface::class));
+				$component->setMVCFactory($container->get(MVCFactoryInterface::class));
+
+				return $component;
+			}
+		);
+	}
+};
---
+++ site/src/Site/Extension/BfstopComponent.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * @package BFStop Component (com_bfstop) for Joomla!
+ * @author Bernhard Froehler
+ * @copyright (C) Bernhard Froehler
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+**/
+
+namespace Codeling\Component\Bfstop\Site\Extension;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\MVCComponent;
+
+class BfstopComponent extends MVCComponent
+{
+}
