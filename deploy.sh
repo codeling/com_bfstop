492d7478c45a3177dfbf634fbfe34d25c757a75a
Bernhard Froehler <bernhard@bfroehler.info>
2026-09-12 23:58:42 +0200

    Fix admin/site split, update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,8 @@ version=1.5.2
 component_name=com_${name}
 site_files=site/*
 admin_files=admin/*
-admlang_files=admlang/*
 zipfile_name=${component_name}-${version}.zip
-src_files="${name}.xml ${site_files} ${admin_files} ${admlang_files} installscript.php"
+src_files="${name}.xml ${site_files} ${admin_files} installscript.php"
 
 if [ "$1" == "zip" ]
 then
@@ -29,4 +28,3 @@ fi
 
 cp -r ${site_files} ${dstdir}/components/${component_name}/
 cp -r ${admin_files} ${dstdir}/administrator/components/${component_name}/
-cp -r ${admlang_files} ${dstdir}/administrator/language/
